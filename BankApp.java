a5a1ed5e218530ef6c6d5b7368e2d9081accdb8e
naveen <naaninaveenkumar21@gmail.com>
2021-01-21 19:54:21 +0530

    update in code of mini project

--- BankApp.java
+++ BankApp.java
@@ -1,4 +1,9 @@
 import java.util.Scanner;
+class CustomException extends Exception{
+     CustomException(String msg){
+	super(msg);
+	}
+}
 abstract class AccountInitialization{
 	static int acccount=0;
 	int accno=1010;
@@ -67,11 +72,12 @@ class Account{
 }	
 
 class MyBank extends AccountInitialization implements Bank {
-	Scanner sc=new Scanner(System.in);
-	int flag=0;
-	int flag1=0; 
 	public void transfer(int fromaccno,int toaccno,int amount)
 	{
+	   int flag=0;
+	   int flag1=0;
+	   int flag2=0;
+	   
 		for(int i=0;i<acccount;i++)
 		{
 			if(fromaccno==ac[i].getaccno())
@@ -79,41 +85,76 @@ class MyBank extends AccountInitialization implements Bank {
 					flag=1;
 					if(ac[i].getamount()<amount)
 					{
+						flag2=1;
 						System.out.println("insufficient for transfering");
 					}
-					else{
-						ac[i].setamount(ac[i].getamount()-amount);
-						if(ac[i].getamount()<500)
-							{
-							System.out.println("maintain minimum amount 500");
-							}
-					}	
+					
 				}
 		}
+	   
+	 try{
 		if(flag==0)
-		{
-		System.out.println("from-account number not found");
+			{
+			throw new CustomException("from-account number not found");
+			}
 		}
+		catch(Exception e){
+		System.out.println(e.toString());
+	        }
 		for(int i=0;i<acccount;i++)
 		{
 
 			if(toaccno==ac[i].getaccno())
 				{
 					flag1=1;
-					ac[i].setamount(ac[i].getamount()+amount);
-					System.out.println("Transfer Transaction done successfully");
 					
 				}
 		}
+		try{
 		if(flag1==0)
 			{
-				System.out.println("to-account number not found");
+			throw new CustomException("to-account number not found");
 			}
-		
+		}
+		catch(Exception e){
+		System.out.println(e.toString());
+	        }
+		if(flag1==1&&flag==1&&flag2==0)
+		{
+			for(int i=0;i<acccount;i++)
+			{
+
+			if(toaccno==ac[i].getaccno())
+			{
+				
+				ac[i].setamount(ac[i].getamount()+amount);
+			}
+			if(fromaccno==ac[i].getaccno())
+			{
+				
+				ac[i].setamount(ac[i].getamount()-amount);
+				try{
+					if(ac[i].getamount()<500)
+					{
+						throw new CustomException("maintain minimum amount 500");
+					}
+				   }
+				catch(Exception e){
+					System.out.println(e.toString());
+	        		  }
+				
+				}
+			}
+			if(flag2==0)
+			{
+			System.out.println("Transfer Transaction done successfully");
+			}
+		}
 	}
 
 	public void withdraw(int accno,int amount){
-		int flag=0;
+	int flag=0;
+	try{
 		for(int i=0;i<acccount;i++)
 		{
 			if(accno==ac[i].getaccno())
@@ -121,6 +162,7 @@ class MyBank extends AccountInitialization implements Bank {
 				flag=1;
 				if(ac[i].getamount()<amount)
 					{
+						
 						System.out.println("amount is not available");
 					}
 				else
@@ -129,38 +171,57 @@ class MyBank extends AccountInitialization implements Bank {
 						System.out.println("Withdraw Transaction done successfully");
 						if(ac[i].getamount()<500)
 						{
-							System.out.println("maintain minimum amount 500");
+							throw new CustomException("maintain minimum amount 500");
 						}
+						
 				   	}
 			}
 		}
-	
+	}
+	catch(Exception e){
+		System.out.println(e.toString());
+	}
+		try{
 		if(flag==0)
-		{
-			System.out.println("account number not found");
+			{
+			throw new CustomException("account number not found");
+			}
 		}
+		catch(Exception e){
+		System.out.println(e.toString());
+	        }
 	}
 		
 
 	public void deposit(int accno,int amount){
 		int flag=0;
+		int flag1=0;
 		for(int i=0;i<acccount;i++)
 		{
-			if(amount>49999)
-			{
-				System.out.println("cannot deposit more than 500");
-			}
 			if(accno==ac[i].getaccno())
 			{
 				flag=1;
+				if(amount>49999)
+				{
+					flag1=1;
+					System.out.println("cannot deposit more than 50000");
+				}
 				ac[i].setamount(ac[i].getamount()+amount);
+				if(flag==1&&flag1==0)
+				{
 				System.out.println("deposit Transaction done successfully");
+				}
 			}
 		}
+		try{
 		if(flag==0)
 			{
-				System.out.println("account number not found");
+			throw new CustomException("account number not found");
 			}
+		}
+		catch(Exception e){
+		System.out.println(e.toString());
+	        }
 		
 	}
 
@@ -175,10 +236,15 @@ class MyBank extends AccountInitialization implements Bank {
 				System.out.println("balance rs."+ac[i].getamount());
 			}
 		}
+		try{
 		if(flag==0)
-		{
-			System.out.println("account number not found");
+			{
+			throw new CustomException("account number not found");
+			}
 		}
+		catch(Exception e){
+		System.out.println(e.toString());
+	        }
 	}
 }
 		
